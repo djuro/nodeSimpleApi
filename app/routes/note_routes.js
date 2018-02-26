6c066e969e3f9c752f7ce4946896f2367dd5e955
djuro <djuro@TopDog>
2018-02-26 23:14:45 +0000

    Initial commit. Added dependencies, one route, database connection and db insert example.

---
+++ app/routes/note_routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(app, db) {
+
+  const collection =
+  app.post('/notes', (req, res)=> {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if(err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+
+  });
+};
